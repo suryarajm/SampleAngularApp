bd5cc73c1d7b38c04c2919ba4a68a2925a04f667
suryar <suryar@qburst.com>
2016-01-22 17:22:40 +0530

    final modifications on friday

--- js/edit.js
+++ js/edit.js
@@ -19,16 +19,6 @@ $(document).on("click", "#backToIndex", function() {
 });
 
 
-/**-------------Controller for index page-----------------**/
- var empStaticApp = angular.module('staticTableApp',[]);
-       empStaticApp.controller('empDataController',function($scope){     
-       	  	 $scope.employees = [{empId:"101",name:"Jose",position:"Engineer",mobile:"9012345678"},
-                              {empId:"102",name:"John",position:"Engineer",mobile:"9210345678"},
-                              {empId:"103",name:"Jacob",position:"Tester",mobile:"9301245678"},
-                              {empId:"104",name:"James",position:"HR",mobile:"9412305678"},
-                              {empId:"105",name:"Jain",position:"HR",mobile:"9512340678"},
-                              {empId:"106",name:"Jijo",position:"Tester",mobile:"9210345678"}];
- });
 
 
 /**-------------Controller for edit page-----------------**/
